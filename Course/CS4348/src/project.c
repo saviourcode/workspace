b453a3d7bb5a8b6853dc16bcf0b6a8c5e687970d
saviourcode <sourabhchoure98@gmail.com>
2021-06-18 21:31:24 -0500

    Project

--- Course/CS4348/src/project.c
+++ Course/CS4348/src/project.c
@@ -11,20 +11,24 @@
 #include <stdlib.h>
 #include <errno.h>
 
+// Enumeration to hold STDIN, STDOUT, STDERR
 enum {STDIN = 0, STDOUT = 1, STDERR = 2};
+// Enumeration to hold READ_END, WRITE_END
 enum {READ_END = 0, WRITE_END = 1};
 
-#define PATH_NAME 20
+// Macros to store total PATH and total number of processes
+#define PATH_NAME 30
 #define NUM_CHILDS 2
 
+// Macros to store file parameters
 #define FILENAME 20
 #define LENGTH 10
-#define BUFFER_SIZE 100
+#define BUFFER_SIZE 4096
 
-/* Dummy files */
-int fdTEST[NUM_CHILDS];
-    
-
+/* Function Description:
+ * Write the given char[] to the specified file descriptor.
+ * If any error occurs then stop the program.
+ */
 void writeFd(int fd, const char *strWrite)
 {
     size_t nbytes;     // Store number of bytes of string
@@ -33,6 +37,8 @@ void writeFd(int fd, const char *strWrite)
     nbytes = strlen(strWrite);
     retnbytes = write(fd, strWrite, nbytes);
 
+
+    // Check for error
     if (retnbytes < 0)
     {
         char *str = "write() Failed!\n";
@@ -43,12 +49,17 @@ void writeFd(int fd, const char *strWrite)
     }
 }
 
-void writeFdPipe(int fd, const char *strWrite, size_t nbytes)
+/* Function Description:
+ * Write the given char[] to the specified file descriptor, and also pass the number of bytes to write
+ * If any error occurs then stop the program.
+ */
+void writeFdBytes(int fd, const char *strWrite, size_t nbytes)
 {
     ssize_t retnbytes; // Store the value returned from read and write
 
     retnbytes = write(fd, strWrite, nbytes);
 
+    // Check for error
     if(retnbytes < 0)
     {
         char *str = "write() Failed!\n";
@@ -59,12 +70,17 @@ void writeFdPipe(int fd, const char *strWrite, size_t nbytes)
     }
 }
 
+/* Function Description:
+ * Read characters into the given char[] from the specified file descriptor, read only the specified number of bytes
+ * If any error occurs then stop the program.
+ */
 ssize_t readFd(int fd, char *buff, size_t nbytes)
 {
     ssize_t retnbytes;
 
     retnbytes = read(fd, buff, nbytes);
 
+    // Check for error
     if (retnbytes < 0)
     {
         char *str = "read() Failed!\n";
@@ -77,11 +93,17 @@ ssize_t readFd(int fd, char *buff, size_t nbytes)
     return retnbytes;
 }
 
+/* Function Description:
+ * Open and fetch the contents of file and write it to the given file descriptor
+ * If any error occurs then stop the program.
+ */
 void fileWriter(const char *fileName, int fd)
 {
     char buff[BUFFER_SIZE] = {0}; // Critical to zero out or else buffer contains random values
 
     int fileDesc;
+    // Check for error
+
     if ((fileDesc = open(fileName, O_RDONLY, NULL)) == -1)
     {
         char strWrite[40];
@@ -94,17 +116,23 @@ void fileWriter(const char *fileName, int fd)
 
     size_t nbytes = strlen(buff);
     char size[LENGTH];
-    snprintf(size, 10, "%u\n", nbytes);
+    snprintf(size, 10, "%lu\n", nbytes);
 
-    writeFdPipe(fd, size, LENGTH);
-    writeFdPipe(fd, buff, BUFFER_SIZE);
+    writeFdBytes(fd, size, LENGTH);
+    if(nbytes!=0)
+        writeFdBytes(fd, buff, BUFFER_SIZE);
 }
 
+/* Function Description:
+ * Open and read the directory listings of the given directory. Also write the names of files to the file descriptor
+ * If any error occurs then stop the program.
+ */
 void dirWriter(const char *dirName, int fd)
 {
     DIR *dirp;
     struct dirent *dp;
 
+    // Check for error
     if ((dirp = opendir(dirName)) == NULL)
     {
         writeFd(STDERR, "opendir() Failed\n");
@@ -123,14 +151,14 @@ void dirWriter(const char *dirName, int fd)
             {
                 char fileName[FILENAME] = {0};
                 strcat(fileName, dp->d_name);
-                strcat(fileName, "\n");
-                writeFdPipe(fd, fileName, sizeof(fileName));
+                writeFdBytes(fd, fileName, sizeof(fileName));
                 strcat(fileLocation, dp->d_name);
                 fileWriter(fileLocation, fd);
             }
         }
     } while (dp != NULL);
 
+    // Check for error
     if (errno != 0)
     {
         writeFd(STDERR, "readdir() Failed\n");
@@ -138,31 +166,53 @@ void dirWriter(const char *dirName, int fd)
     }
 }
 
-void dirReader(const char *dirName, int fd, int i)
+/* Function Description:
+ * Read the contents from the sibling's pipe and then create new files with the respective contents
+ * If any error occurs then stop the program.
+ */
+void dirReader(const char *dirName, int fd)
 {
     char fileName[FILENAME] = {0};
     char size[LENGTH] = {0};
     char buffer[BUFFER_SIZE] = {0};
-    
+    size_t retSize;
+
     while(readFd(fd, fileName, FILENAME))
     {
-        writeFd(fdTEST[i], fileName);
+        char fileDirPath[PATH_NAME] = {0};
+        strcat(fileDirPath,dirName);
+        strcat(fileDirPath,fileName);
+
         readFd(fd, size, LENGTH);
-        writeFd(fdTEST[i], size);
-        if(strcmp(size,"0") == 0)
+
+        retSize = strtol(size,NULL,10);
+        if(retSize== 0)
             continue;
+
         readFd(fd, buffer, BUFFER_SIZE);
-        writeFd(fdTEST[i], "Now reading buffer\n");
-        writeFd(fdTEST[i], buffer);
+
+        // Check for error
+        if(access(fileDirPath, F_OK) == 0)
+                writeFd(STDOUT,"File already exists! Won't overwrite\n");
+        else
+        {
+            int fdwr = open(fileDirPath, O_WRONLY | O_TRUNC | O_CREAT | O_EXCL, 0644);
+            write(fdwr,buffer,retSize);
+            close(fdwr);
+        }
     }
-        
 }
 
+/* Function Description:
+ * Creates two ordinary pipe and then forks two child processes with their respective directory to handle.
+ * The two child processes, first closes the unused end of the pipe, and then start writing the whole directory 
+ * to the respective write end of the pipe. Once writing is done, then close the write end of the self pipe and start
+ * reading from the sibling's pipe.
+ * Parent closes all the pipes and waits for the child process to complete.
+ * If any error occurs then stop the program.
+ */
 int main()
 {
-    fdTEST[0] = open("./proc1", O_RDWR | O_TRUNC, NULL);
-    fdTEST[1] = open("./proc2", O_RDWR | O_TRUNC, NULL);
-
     char *dirNames[NUM_CHILDS] = {"./dir1/", "./dir2/"};
 
     // Make pipes
@@ -171,6 +221,7 @@ int main()
 
     for(size_t i = 0; i < NUM_CHILDS; i++)
     {
+        // Check for error
         if((status = pipe(fildesc[i])) == -1)
         {
             writeFd(STDERR, "pipe() Failed!\n");
@@ -186,26 +237,29 @@ int main()
         // fork a child process
         pidChild[i] = fork();
 
-        // error occured
+        // Check for error
         if(pidChild[i] < 0) { 
             writeFd(STDERR, "fork() Failed!\n");
             exit(EXIT_FAILURE);
         }
         // child process
         else if(pidChild[i] == 0){
-            // do something with pipes
-            writeFd(STDERR, "I am a child!\n");
+            // Close the Read End self pipe
             close(fildesc[i][READ_END]);
+            // Close the Write End of sibling's pipe
             close(fildesc[1-i][WRITE_END]);
 
-            //dirWriter(dirNames[i], fd[i]);
-
+            // Start writing the contents of directory to the pipe
             dirWriter(dirNames[i], fildesc[i][WRITE_END]);
+            // Close the Write End of self pipe
             close(fildesc[i][WRITE_END]);
 
-            dirReader(dirNames[i], fildesc[1-i][READ_END],i);
+            // Start reading the contents of sibling's pipe
+            dirReader(dirNames[i], fildesc[1-i][READ_END]);
+            // Close the Read End of sibling's pipe
             close(fildesc[i-1][READ_END]);
 
+            // destroy the child process
             exit(EXIT_SUCCESS);
         }
     }
@@ -220,6 +274,7 @@ int main()
     // Wait for Child Processes to complete
     for(size_t i = 0; i < NUM_CHILDS; i++)
     {
+        // Check for error
         if(wait(NULL) == -1)
         {
             writeFd(STDERR, "wait() Failed!\n");
